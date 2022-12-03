ad9e476f56b1f13dd7493ea85546c93a8e6ab1f0
Sondre <datskalf@outlook.com>
2022-12-03 17:02:09 +0000

    Create main.py

---
+++ 2022/Day 1/main.py
@@ -0,0 +1,17 @@
+with open("2022/Day 1/data.txt", "r") as file:
+    data = file.read()
+
+sums = []
+
+index = 0
+for line in data.split('\n'):
+    if len(line) == 0:
+        index += 1
+        continue
+    if len(sums) == index:
+        sums.append(0)
+    sums[index] += int(line)
+
+sums.sort(reverse=True)
+print(f"Part 1: {sums[0]}")
+print(f"Part 2: {sums[0] + sums[1] + sums[2]}")
