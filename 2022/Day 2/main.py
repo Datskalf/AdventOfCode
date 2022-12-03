eba71b765b4d5bbd19603913eb47332ecdad5a3e
Sondre <datskalf@outlook.com>
2022-12-03 17:11:43 +0000

    Update Day 2 Answer
    Included part 1 answer in the main.py file

--- 2022/Day 2/main.py
+++ 2022/Day 2/main.py
@@ -1,26 +1,43 @@
-with open("2022/Day 2/data.txt", "r") as file:
+INPUTFILE = "2022/Day 2/data.txt"
+
+with open(INPUTFILE, "r") as file:
     data = file.read()
 
-totalScore = 0
+
 
 points = {
-    "A X": 3,
-    "A Y": 4,
-    "A Z": 8,
-    "B X": 1,
-    "B Y": 5,
-    "B Z": 9,
-    "C X": 2,
-    "C Y": 6,
-    "C Z": 7
+    "part1": {
+        "A X": 4,
+        "A Y": 8,
+        "A Z": 3,
+        "B X": 1,
+        "B Y": 5,
+        "B Z": 9,
+        "C X": 7,
+        "C Y": 2,
+        "C Z": 6
+    },
+    "part2": {
+        "A X": 3,
+        "A Y": 4,
+        "A Z": 8,
+        "B X": 1,
+        "B Y": 5,
+        "B Z": 9,
+        "C X": 2,
+        "C Y": 6,
+        "C Z": 7
+    }
 }
 
-for i, line in enumerate(data.split("\n")):
-    roundScore = 0
 
-    roundScore = points[line]
-    
-    print(f"Game {i}: {roundScore}")
-    totalScore += roundScore
 
-print(f"\nTotal score: {totalScore}")
+part1 = 0
+part2 = 0
+
+for line in data.split("\n"):
+    part1 += points["part1"][line]
+    part2 += points["part2"][line]
+
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
